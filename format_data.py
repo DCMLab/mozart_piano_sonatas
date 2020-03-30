28b48970d0158751dc7c915dc0312cff4a78152f
laserjeyes <johannes.hentschel@epfl.ch>
2020-03-30 19:10:00 +0200

    renaming

--- format_data.py
+++ format_data.py
@@ -1,3 +1,10 @@
+"""
+Internal note: How to derive this data structure from the (MS3) MSCX files:
+1. python extract_annotations.py -cnmo
+2. rename folder chords -> harmonies
+3. Delete files with ambiguous_measure_numbers
+"""
+
 import argparse, os, sys
 from fractions import Fraction as frac
 from functools import reduce
@@ -11,20 +18,32 @@ python -m pip install pandas""")
 os.environ['NUMEXPR_MAX_THREADS'] = '64' # to silence NumExpr prompt
 idx = pd.IndexSlice                      # for easy MultiIndex slicing
 
+LOSTR2INT = lambda l: '' if pd.isnull(l) else l.split(', ')
+
 CONVERTERS = {
+    'act_dur': frac,
+    'next': LOSTR2INT,
+    'nominal_duration': frac,
+    'offset': frac,
     'onset': frac,
     'duration': frac,
-    'nominal_duration': frac,
     'scalar': frac,}
 
 DTYPES = {
+    'barline': str,
+    'dont_count': 'Int64',
+    'keysig': int,
     'mc': int,
-    'mn': int,
-    'tied': 'Int64',
-    'tpc': int,
     'midi': int,
+    'mn': int,
+    'numbering_offset': 'Int64',
+    'repeats': str,
     'staff': int,
+    'tied': 'Int64',
+    'timesig': str,
+    'tpc': int,
     'voice': int,
+    'voices': int,
     'volta': 'Int64',
 }
 
@@ -86,36 +105,18 @@ FILE_LIST = pd.DataFrame({'filename':
 
 FILE_LIST.index.names = ['sonata', 'movement']
 
-# def pair2list(pair):
-#     """Takes a range such as (2,4) and turns it into the corresponding list [2,3,4]"""
-#     fr, to = pair
-#     fr, to = int(fr), int(to)
-#     if to < fr:
-#         fr, to = to, fr
-#     if fr < 1:
-#         fr = 1
-#     if to > 18:
-#         to = 18
-#     return list(range(fr, to+1))
-#
-# def treatval(val):
-#     """ Turns a parameter into a list of int."""
-#     try:
-#         val = [int(val)]
-#     except:
-#         if val.__class__ == tuple and len(sonatas) == 2:
-#             val = pair2list(val)
-#     return [int(i) for i in val if 0 < int(i) < 19]
 
 
-
-
-
-def read_tsvs(dir, selection):
+def read_tsvs(dir, selection, unfold):
     files = (selection.filename + '.tsv').values
-    return pd.concat([pd.read_csv(os.path.join(dir, f), sep='\t', dtype=DTYPES, converters=CONVERTERS) for f in files],
+    df = pd.concat([pd.read_csv(os.path.join(dir, f), sep='\t', dtype=DTYPES, converters=CONVERTERS) for f in files],
                      keys=selection.filename)
-
+    if unfold:
+        return repeat(df) # groupby-apply
+    else:
+        if 'volta' in df.columns:
+            df = df[(df.volta != 1).fillna(True)].drop(columns='volta')
+        return df
 
 
 
@@ -151,9 +152,77 @@ def store_result(df, fname, what):
     tsv_name = f"{fname}_{what}.tsv" if what != 'joined' else fname + '.tsv'
     tsv_path = os.path.join(args.dir, tsv_name)
     df.to_csv(tsv_path, sep='\t')
-    print(f"PREVIEW of {tsv_path}:", notes.head(1), sep='\n', end='\n\n')
+    print(f"PREVIEW of {tsv_path}:", df.head(1), sep='\n', end='\n\n')
 
 
+
+def format_data(name=None, dir=None, unfold=False, sonatas=None, movements=None, test=False, notes=False, harmonies=False, cadences=False, measures=False, join=False, propagate=False):
+    fname = ' '.join(sys.argv[1:]) if name is None else name
+
+    if dir is None:
+        dir = os.path.join(os.getcwd(), 'formatted')
+
+    selection = select_files(sonatas=sonatas, movements=movements)
+
+    if test:
+        print(selection)
+    elif not harmonies and not cadences and not notes:
+        print("Select the kind of data: -N for notes, -H for harmony labels, and -C for cadence labels. Pass -j to join several kinds into a single TSV.")
+    elif len(selection) == 0:
+        print("No data matching your selection.")
+    else:
+        script_path = os.path.abspath('')
+        kinds = []
+        for kind in ['notes', 'harmonies', 'cadences', 'measures']:
+            if locals()[kind]:
+                kinds.append(kind)
+        if join:
+            assert len(kinds) > 1, "Select at least two kinds of data for joining."
+
+        joining = {kind: read_tsvs(os.path.join(script_path, kind), selection, unfold) for kind in kinds}
+        return joining
+
+        if join:
+            for df in joining:
+                df.index = df.index.droplevel(1)
+            joined = reduce(lambda left,right: pd.merge(left, right, left_index=True, right_index=True, how='outer', suffixes=('', '_y')), joining)
+            duplicates = [col for col in joined.columns if col.endswith('_y')]
+            joined.drop(columns=duplicates, inplace=True)
+            if propagate:
+                if cadences:
+                    joined.cadence.fillna(method='bfill', inplace=True)
+                if notes and harmonies:
+                    joined.chords.fillna(method='ffill', inplace=True)
+            store_result(joined, fname, 'joined')
+        else:
+            for kind, tsv in joining.items():
+                store_result(tsv, fname, kind)
+
+
+# joining = format_data(notes=True, harmonies=True, cadences=True, measures=True)
+
+def join_tsv(notes=None, harmonies=None, cadences=None, measures=None):
+    """"""
+    if notes is not None:
+        if harmonies is not None:
+            notes = notes.drop(columns='mn')
+            left = pd.merge(notes, harmonies, on=['filename', 'mc', 'onset'], how='outer', suffixes=('', '_y'))\
+                          .sort_values(['mc', 'onset'])\
+                          .drop(columns='mn')
+            left = pd.merge(left, measures[['mc', 'mn']], on=['filename', 'mc'])
+            return left
+        else:
+            left = notes
+    else:
+        left = harmonies
+    # if cadences is not None:
+    #     if
+
+
+# joining['measures'].offset.value_counts()
+# join_tsv(**joining)
+# test[(test.volta != 1).fillna(True)]
+# test[test.mn.isna()]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -163,64 +232,31 @@ if __name__ == '__main__':
 | Format Mozart data to your needs |
 ------------------------------------
 
-Run this script at the top level of (your local copy of) the repo  mozart_piano_sonatas.
+This script needs to remain at the top level of (your local copy of) the repo  mozart_piano_sonatas.
 ''')
     parser.add_argument('name', metavar='NAME', nargs='?', type=check_dir, help='You may choose a name for the new TSV file(s). Existing files will be overwritten.')
     parser.add_argument('dir', metavar='DIR', nargs='?', type=check_dir, default=os.path.join(os.getcwd(), 'formatted'), help='Folder for storing the new TSV file(s). Can be relative, defaults to ./formatted')
+    parser.add_argument('-u','--unfold', action='store_true', help="Unfold: Repeat everything that is repeated in the score, taking into account first and second endings ('voltas'). Otherwise, only second endings are returned.")
     parser.add_argument('-s','--sonatas', nargs='+', type=int, help="Select sonatas out of 1-18, e.g. -s 2 5 12")
     parser.add_argument('-m','--movements', nargs='+', type=int, help="Select only movements 1, 2 or 3, e.g. -m 1 3")
     parser.add_argument('-t','--test', action='store_true', help="Only test/view file selection without storing any data. Use -t without -sm to view all files.")
-    parser.add_argument('-H','--harmonies', action='store_true', help="Get chord labels.")
-    parser.add_argument('-C','--cadences', action='store_true', help="Get cadence labels.")
     parser.add_argument('-N','--notes', action='store_true', help="Get note lists.")
+    parser.add_argument('-H','--harmonies', action='store_true', help="Get harmony labels.")
+    parser.add_argument('-C','--cadences', action='store_true', help="Get cadence labels.")
+    parser.add_argument('-M','--measures', action='store_true', help="Get measure properties.")
     parser.add_argument('-j','--join', action='store_true', help="Join the data into one single TSV.")
     parser.add_argument('-p','--propagate', action='store_true', help="When joining, spread out chord and cadence labels.")
     args = parser.parse_args()
 
-    selection = select_files(sonatas=args.sonatas, movements=args.movements)
-    if not args.harmonies and not args.cadences and not args.notes:
-        print("Select the kind of data: -N for notes, -H for harmony labels, and -C for cadence labels. Pass -j to join several kinds into a single TSV.")
-    elif len(selection) == 0:
-        print("No data matching your selection.")
-    elif args.test:
-        print(selection)
-    else:
-        fname = ' '.join(sys.argv[1:]) if args.name is None else args.name
-        script_path = os.path.realpath(os.path.dirname(__file__))
-        if args.join:
-            joining = []
-            assert sum([args.notes, args.harmonies, args.cadences]) > 1, "Select at least two kinds of data for joining."
-
-        if args.notes:
-            notes = read_tsvs(os.path.join(script_path, 'labels/notes'), selection)
-            if args.join:
-                joining.append(notes.set_index(['mn', 'onset'], append=True))
-            else:
-                store_result(notes, fname, 'notes')
-
-        if args.harmonies:
-            chords = read_tsvs(os.path.join(script_path, 'labels/chords'), selection)
-            if not args.join:
-                store_result(chords, fname, 'chords')
-            else:
-                joining.append(chords.set_index(['mn', 'onset'], append=True))
-
-        if args.cadences:
-            cadences = read_tsvs(os.path.join(script_path, 'labels/cadences'), selection)
-            if not args.join:
-                store_result(cadences, fname, 'cadences')
-            else:
-                joining.append(cadences.set_index(['mn', 'onset'], append=True))
-
-        if args.join:
-            for df in joining:
-                df.index = df.index.droplevel(1)
-            joined = reduce(lambda left,right: pd.merge(left, right, left_index=True, right_index=True, how='outer', suffixes=('', '_y')), joining)
-            duplicates = [col for col in joined.columns if col.endswith('_y')]
-            joined.drop(columns=duplicates, inplace=True)
-            if args.propagate:
-                if args.cadences:
-                    joined.cadence.fillna(method='bfill', inplace=True)
-                if args.notes and args.harmonies:
-                    joined.chords.fillna(method='ffill', inplace=True)
-            store_result(joined, fname, 'joined')
+    format_data(args.name,
+                args.dir,
+                args.unfold,
+                args.sonatas,
+                args.movements,
+                args.test,
+                args.notes,
+                args.harmonies,
+                args.cadences,
+                args.measures,
+                args.join,
+                args.propagate)
